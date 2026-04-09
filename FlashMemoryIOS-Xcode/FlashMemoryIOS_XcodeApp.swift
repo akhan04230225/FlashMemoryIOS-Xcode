7c492dc97d8278d5b448b2ca271f2a335b1461e6
Ahmed Khan <aik2004umbc@gmail.com>
2026-04-09 11:34:28 -0400

    Initial Commit

---
+++ FlashMemoryIOS-Xcode/FlashMemoryIOS_XcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashMemoryIOS_XcodeApp.swift
+//  FlashMemoryIOS-Xcode
+//
+//  Created by Ahmed Khan on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct FlashMemoryIOS_XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
